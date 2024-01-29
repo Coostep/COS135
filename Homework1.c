7a67c59fc8c42d070c3a3dfebe7f6de97acc34a6
Cooper Stepankiw <147639780+Coostep@users.noreply.github.com>
2024-01-29 14:30:54 -0500

    Add files via upload

---
+++ Homework1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+
+//printf() displays a string inside quotations
+// \n adds a new line at the end of a string
+
+	printf("Hello, World!\n\n");
+//1.)
+	printf("Hello,\nUmaine!\n\n");
+//2.)
+	printf("    *\n   **\n  ***\n ****\n*****\n\n");
+//3.)
+	printf("*****\n *** \n  *  \n\n");
+//4.)
+	printf("|\\\n| \\\n|  \\\n|___\\\n\n");
+//5.)
+	printf("   * *      *\n\n *      * *\n\n*\n\n");
+	return 0;
+}
